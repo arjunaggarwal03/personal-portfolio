b66420509485bbb5e8491720c99bdc34d5f8ecef
arjun aggarwal <arjunaggarwal173@gmail.com>
2024-06-17 03:31:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,16 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        hi, i'm arjun 👨🏽‍💻
+        hi, i'm arjun 🌆
       </h1>
       <p className="mb-4">
-        {`i'm an honors cs + math major at umd with experience at capital one, bank of america, and currently amazon. 
+        {`i'm an honors cs + math major at umd with experience at capital one, bank of america, and currently amazon.
         outside of work and school, you can find me busy with alpha kappa psi events, watching some a24 movie, or building an obscure spotify playlist.
-        also, i've been building plato (check it out) with my friend rohan.`}
+        also, i've been building `}
+        <a href="https://platoportal.com/" target="_blank" rel="noopener noreferrer">
+          plato
+        </a>
+        {` with my friend rohan. give it a look and let me know what you think 😁 `}
       </p>
       <div className="my-8">
         <BlogPosts />
