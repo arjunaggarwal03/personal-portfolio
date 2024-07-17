3a667f7eecc4e2e1c7de8b21a18024ac43f30190
arjun aggarwal <arjunaggarwal173@gmail.com>
2024-07-16 18:15:32 -0700

    update footer, increase width

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './global.css';
 import type { Metadata } from 'next';
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -11,15 +9,15 @@ import { baseUrl } from './sitemap';
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Arjun\'s Portfolio',
-    template: 'Arjun\'s Portfolio',
+    default: "Arjun's Portfolio",
+    template: "Arjun's Portfolio",
   },
-  description: 'This is Arjun Aggarwal\'s personal website.',
+  description: "This is Arjun Aggarwal's personal website.",
   openGraph: {
-    title: 'Arjun\'s Portfolio',
-    description: 'This is Arjun\'s portfolio.',
+    title: "Arjun's Portfolio",
+    description: "This is Arjun's portfolio.",
     url: baseUrl,
-    siteName: 'Arjun\'s Portfolio',
+    siteName: "Arjun's Portfolio",
     locale: 'en_US',
     type: 'website',
   },
@@ -48,16 +46,13 @@ export default function RootLayout({
       lang="en"
       className={cx(
         'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
       )}
     >
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" />
       </head>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased mx-4 mt-8 lg:mx-auto font-sans">
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 container">
           <Navbar />
           {children}
           <Footer />
