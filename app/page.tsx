7f3884ce23650facfacc356071d9e69f0066ee16
arjun aggarwal <arjunaggarwal173@gmail.com>
2025-01-27 06:40:40 -0500

    add painting, add easter egg

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,16 @@ export default function Page() {
         Hi, I'm Arjun 🌆
       </h1>
       <p>
-        {`I'm an incoming software engineer at YouTube. I'm also a fourth-year Honors CS + Math major at the University of Maryland with dev experience at Amazon Web Services, Capital One, and Bank of America. Here's some stuff I've worked on:`}
+        I'm an incoming software engineer at{' '}
+        <a 
+          href="https://youtu.be/xFBKXQILeqI?si=rStU-oCYDlbOQp20"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-[#FF0000] transition-colors duration-200"
+        >
+          YouTube
+        </a>
+        . I'm also a fourth-year Honors CS + Math major at the University of Maryland with dev experience at Amazon Web Services, Capital One, and Bank of America. Here's some stuff I've worked on:
       </p>
       <div>
         <BlogPosts />
